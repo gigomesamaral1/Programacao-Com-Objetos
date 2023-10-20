5347c2e1a576a81ddd8ec767cf048b65c24aeb86
gigomesamaral1 <101269031+gigomesamaral1@users.noreply.github.com>
2023-10-20 10:31:48 -0300

    AC05 20.10.2023

---
+++ AC05/AlunoDAO.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlunoDAO {
+    private List<Aluno> alunos;
+
+    public AlunoDAO() {
+        alunos = new ArrayList<>();
+    }
+
+    public void adicionarAluno(String matricula, String nome, String curso) {
+        Aluno aluno = new Aluno(matricula, nome, curso);
+        alunos.add(aluno);
+    }
+
+    public void removerAluno(String matricula) {
+        alunos.removeIf(a -> a.getMatricula().equals(matricula));
+    }
+
+    public void atualizarCurso(String matricula, String novoCurso) {
+        for (Aluno aluno : alunos) {
+            if (aluno.getMatricula().equals(matricula)) {
+                aluno.setCurso(novoCurso);
+                return;
+            }
+        }
+    }
+
+    public Aluno listarAlunoPorMatricula(String matricula) {
+        for (Aluno aluno : alunos) {
+            if (aluno.getMatricula().equals(matricula)) {
+                return aluno;
+            }
+        }
+        return null;
+    }
+
+    public List<Aluno> listarTodosAlunos() {
+        return alunos;
+    }
+}
